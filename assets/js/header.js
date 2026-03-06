ca6a870c8f4c2fc098e5b0ae32c41e23bd344188
pandxsah <pandxsah@gmail.com>
2026-03-06 12:21:03 +0530

    Delete assets directory

--- assets/js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click", (e) => {
-  const menuButton = e.target.closest("#menuButton");
-  const mobileMenu = document.getElementById("mobileMenu");
-
-  if (!menuButton || !mobileMenu) return;
-
-  mobileMenu.classList.toggle("hidden");
-});
